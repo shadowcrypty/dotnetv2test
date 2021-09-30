740be13a3c7be74d6c07244ad63b35d46e998301
DESKTOP-HSGUA6V\Anarchy <ziyanamli@gmail.com>
2021-09-30 06:14:17 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace project_consol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+
+        }
+    }
+}
